7e6dc0a64e4c81dc7904d366a15511c541518b16
rasani8064 <81849484+rasani8064@users.noreply.github.com>
2021-07-08 06:50:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let throttle = (fn, delay) => {
+  let last = 0;
+  return (...args) => {
+    const now = new Date().getTime();
+    if (now - last < delay) {
+      return;
+    }
+    last = now;
+    return fn(...args);
+  };
+};
+
+document.getElementById("clickMe").addEventListener(
+  "click",
+  throttle((e) => {
+    console.log("Helo");
+  }, 5000)
+);
